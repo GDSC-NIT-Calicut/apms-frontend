c6da2d642cc62ba6ed9d5f26fc98a31086cfe6a0
Aditya-Sh77 <aditya.sh775@gmail.com>
2025-06-28 02:36:25 +0530

    Added profile

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Sidebar from "./components/Sidebar";
+import Profile from "./components/Profile";
 
 function App() {
   return (
     <div className="flex">
       <Sidebar />
-      <div className="flex-1 p-8">
+      <Profile compact/> {/* remove compact to switch to full profile view and vice-versa */}
+      {/* <div className="flex-1 p-8">
         <h1 className="text-2xl font-bold">Main Content</h1>
-      </div>
+      </div> */}   
     </div>
   );
 }
