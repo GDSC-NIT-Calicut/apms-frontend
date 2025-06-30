651524e55f2012bb580f5d3711faefb24ffd839a
Nandini <nandinimprakash.06@gmail.com>
2025-06-30 16:09:41 +0530

    Implemented login page

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -6,12 +6,17 @@ export default function Sidebar() {
   return (
     <div className="w-64 min-h-screen bg-gradient-to-r from-[rgba(226,69,61,0.15)] to-[rgba(85,127,223,0.2)] relative">
       <div className="absolute top-0 right-0 w-[2px] h-full bg-[rgba(38,134,255,0.4)]" />
+      <div
+        className="absolute left-0 w-full h-[1.5px] bg-[rgba(38,134,255,0.2)]"
+        style={{ top: '80px' }}
+      />
+
       <nav className="flex flex-col gap-4 mt-[25vh]">
         {navItems.map((item) => (
           <button
             key={item}
             onClick={()=>setSelected(item)}
-            className={`w-full text-center py-4 px-4 transition font-redhat font-semibold
+            className={`w-full text-center py-4 px-4 transition font-redhat font-semibold cursor-pointer
             ${selected === item
             ? "bg-[#1A3B66] text-white"
             : "text-white hover:bg-white/10"}`}
