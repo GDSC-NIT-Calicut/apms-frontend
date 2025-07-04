8517220996328dabe98f7fe3e04be9bed83a48fb
Aditya-Sh77 <aditya.sh775@gmail.com>
2025-07-05 03:29:45 +0530

    Implemented event manager dashboard

--- src/pages/EventDashboard.tsx
+++ src/pages/EventDashboard.tsx
@@ -1,88 +1,77 @@
-import { useEffect, useState } from 'react';
-import Profile from '../components/Profile';
-import { useNavigate} from 'react-router-dom';
+import {useState, useEffect} from 'react'
+import { useNavigate } from 'react-router-dom';
+import Profile from '../components/Profile'
 
 const applicationButtons = [
-  { label: 'Submit Requests', route: '/submit-event-request' },
+  { label: 'Submit Requests', route: '/submit-requests' },
   { label: 'Allocated Requests', route: '/allocated-requests' },
-  { label: 'Revoked Allocation', route: '/revoked-allocation' }
+  { label: 'Revoked Allocations', route: '/revoked-allocations' },
 ];
 
-type ActivityData = {
-  required: number;
-  earned: number;
-  instituteCount: number;
-  departmentCount: number;
-  eligible: boolean;
-};
-
-type StudentProfile = {
-  name: string;
-  rollNo: string;
-  department: string;
-  faAssigned: string;
-};
+type OrganizerProfile = {
+    name: string,
+    rollno: string,
+    department: string,
+    faAssigned: string;
+}
 
 type DashboardData = {
-  profile: StudentProfile;
-  activity: ActivityData;
-};
+    profile: OrganizerProfile,
+}
+type OrganizerDashboardProps = {
+     setIsLoggedIn: (val:boolean) => void;
+}
 
-type DashboardProps = {
-  setIsLoggedIn: (val:boolean) => void;
-};
+export default function OrganizerDashboard( {setIsLoggedIn}: OrganizerDashboardProps) {
+    const [data, setData] = useState<DashboardData | null>(null);
+    const [loading, setLoading] = useState(true);
 
-export default function EventDashboard({setIsLoggedIn}:DashboardProps) {
-  const [data, setData] = useState<DashboardData | null>(null);
-  const [loading, setLoading] = useState(true);
+    const navigate = useNavigate();
 
-  const navigate = useNavigate();
+    useEffect(() => {
+        fetch('/dashboard.json')
+        .then(res => res.json())
+        .then((data: DashboardData) => {
+            setData(data);
+            setLoading(false);
+        })
+        .catch(err => {
+            console.error("Failed to fetch dashboard data", err);
+            setLoading(false);
+        });
+    }, []);
 
-  useEffect(() => {
-    fetch('/dashboard.json')
-      .then(res => res.json())
-      .then((data: DashboardData) => {
-        setData(data);
-        setLoading(false);
-      })
-      .catch(err => {
-        console.error("Failed to fetch dashboard data", err);
-        setLoading(false);
-      });
-  }, []);
+    if (loading) return <div className="text-white p-4">Loading Dashboard...</div>;
+    if (!data) return <div className="text-red-400 p-4">Error loading dashboard</div>;
+    return (
+        <>
+            <header className="w-full overflow-hidden relative top-0 max-h-inherit">
+                <Profile setIsLoggedIn={setIsLoggedIn} user="Event Organizer"/>
+            </header>
+            <main className="w-full mt-15 mb-23">
+                <section className='ml-[20%]'>
+                    <h2 className="text-2xl font-bold text-white mb-10 ml-5 mt-5">Applications</h2>
+                    <nav className="flex flex-col mb-10 ml-5 gap-5 max-w-xl ">
+                        {applicationButtons.map(({ label, route }) => (
+                        <button
+                            key={label}
+                            onClick={() => navigate(route)}
+                            className="w-80 rounded-2xl px-6 py-3 text-white font-semibold transition-all bg-gradient-to-r from-[#E2453D] to-[#557FDF] hover:opacity-90 hover:cursor-pointer"
+                        >
+                            {label}
+                        </button>
+                        ))}
+                    </nav>
+                </section>    
+            </main>
+            <footer className="absolute bottom-0 mt-auto px-6 py-[32px] bg-gradient-to-b from-[#241515] to-[#141a2e] border-t-[2px] border-[rgba(38,134,255,0.4)] w-full min-h-17 ">
+                <div className="absolute top-[-2px] left-[-2px]">
+                <div className={'w-45 h-6 bg-[rgba(38,134,255,0.4)]'} style={{ clipPath: 'polygon(0 0, 0 100%, 100% 0)' }}>
+                    <div className={'relative bottom-[2px] w-45 h-6 bg-black'} style={{ clipPath: 'polygon(0 0, 0 100%, 100% 0)' }} />
+                </div>
+                </div>
+            </footer>
+        </>    
 
-  if (loading) return <div className="text-white p-4">Loading Dashboard...</div>;
-  if (!data) return <div className="text-red-400 p-4">Error loading dashboard</div>;
-
-  return (
-    <div className="flex flex-col">
-      <header className="w-full overflow-hidden">
-        <Profile setIsLoggedIn={setIsLoggedIn}/>
-      </header>
-      <main className="flex mt-2 ml-40 mr-40 gap-[20%]">
-        <section className="p-6">
-          <h2 className="text-2xl font-bold text-white mb-10 ml-5 mt-5">Applications</h2>
-          <nav className="flex flex-col gap-8 max-w-2xl ">
-            {applicationButtons.map(({ label, route }) => (
-              <button
-                key={label}
-                onClick={() => navigate(route)}
-                className="w-60 rounded-2xl px-6 py-3 text-white font-semibold transition-all bg-gradient-to-r from-[#E2453D] to-[#557FDF] hover:opacity-90 hover:cursor-pointer"
-              >
-                {label}
-              </button>
-            ))}
-          </nav>
-        </section>
-      </main>
-
-      <footer className="relative px-6 py-[32px] bg-gradient-to-b from-[#241515] to-[#141a2e] border-t-[2px] border-[rgba(38,134,255,0.4)] w-full min-h-16">
-        <div className="absolute top-[-2px] left-[-2px]">
-          <div className={'w-45 h-6 bg-[rgba(38,134,255,0.4)]'} style={{ clipPath: 'polygon(0 0, 0 100%, 100% 0)' }}>
-            <div className={'relative bottom-[2px] w-45 h-6 bg-black'} style={{ clipPath: 'polygon(0 0, 0 100%, 100% 0)' }} />
-          </div>
-        </div>
-      </footer>
-    </div>
-  )
+    )
 }
